7924f4e0a36df9f7bd6646fd314433cbdd6fd128
MarcoPonchia <ponchia.marco1994@gmail.com>
2022-01-29 16:32:35 +0100

    add enemy with text

--- Assets/Scripts/EnemiesManager.cs
+++ Assets/Scripts/EnemiesManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using Random = UnityEngine.Random;
 
-public class EnemiesManager : MonoBehaviour
+
+public class EnemiesManager : MonoBehaviourWithGameManager
 {
+    [SerializeField]
+    private List<EnemyAI> enemies = new List<EnemyAI>();
+    [SerializeField]
+    private EnemyAI enemy;
+    [SerializeField]
+    private float spawnTime = 2f;
+    [SerializeField]
+    private Transform spawnOrigin;
+    [SerializeField]
+    private float range = 5;
+    [SerializeField]
+    private Transform exit;
+    [SerializeField]
+    private PhraseScriptable testPhrase;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // enemies.Initialize();
+        Observable.Interval(TimeSpan.FromSeconds(spawnTime)).Subscribe((_) => {
+            Spawn();
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    void Spawn()
     {
+        float locationX = Random.Range(spawnOrigin.position.x - range, spawnOrigin.position.x + range);
+        Vector3 position = new Vector3(locationX, spawnOrigin.position.y, spawnOrigin.position.z);
         
+        EnemyAI newEnemy = Instantiate(enemy, position, spawnOrigin.rotation);
+        // newEnemy.phrase = gameManager.PhraseRepository.GetPhrase();
+        newEnemy.phrase = testPhrase;
+        newEnemy.target = exit;
+        enemies.Add(newEnemy);
     }
 }
--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
 
 public class EnemyAI : MonoBehaviour
 {
     public bool killed = false;
-    public NavMeshAgent agent;
+    [SerializeField]
+    private NavMeshAgent agent;
+    [SerializeField]
     public Transform target;
+    [SerializeField]
+    public PhraseScriptable phrase;
+    [SerializeField]
+    public TMP_Text text;
+    [SerializeField]
+    private EnemiesManager manager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        text.text = phrase.Phrase;
     }
 
     // Update is called once per frame
